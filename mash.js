9e6dd44589f5dac23f52a3365ae48de903691779
maximusrASC6 <max.ultimaterealm@gmail.com>
2019-07-12 13:36:40 -0400

    creating a word game through functions

--- mash.js
+++ mash.js
@@ -1,15 +1,25 @@
-function mash() {
+ function mash() {
 
-    return " You will live in " + getHome(place)
+     return " You will live in a " + getHome(place) + ", and you will have " + getChildrenCount(count) + ", kids from" 
 
+ }
+ let places = ["Mansion", "Apartment", "Shack", "House"]
+     places = places[Math.floor(Math.random()*places.length)];
+
+ function getHome(places){
+      return places
 }
-let places = ["Mansion", "Apartment", "Shack", "House"]
-    places = places[Math.floor(Math.random()*places.length)];
+let count = Math.floor((Math.random()*100) + 0)
 
-function getHome(places){
-     return places
-}
-    
-let place = getHome(places)
-let result = mash();
-console.log(result + place);
+ function getChildrenCount(count) {
+     return count
+ }
+
+
+
+ let child = getChildrenCount(count)
+ let place = getHome(places)
+ let result = mash();
+console.log(result, place, child);
+
+
